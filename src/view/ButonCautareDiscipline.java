7ef2e107176801509b0222e2f8088e2bf52f7f0e
cristivalea <160530571+cristivalea@users.noreply.github.com>
2025-02-26 20:38:01 +0200

    Creare butoane adaugare

--- src/view/ButonCautareDiscipline.java
+++ src/view/ButonCautareDiscipline.java
@@ -4,12 +4,13 @@ import model.Disciplina;
 import model.Repository;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class ButonCautareDiscipline extends JButton implements Comand {
     private JTextField txtNumeButon;
     private JLabel labelAfisareDiscipline;
     private JComboBox<String> comboBox;
+    private Disciplina disciplinaSelectata;
+
 
     public ButonCautareDiscipline(JTextField nume, JLabel afisare){
         super("Cautare disiciplina");
@@ -24,13 +25,17 @@ public class ButonCautareDiscipline extends JButton implements Comand {
             String numeDisciplina = this.txtNumeButon.getText();
             DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
             boolean disciplinaGasit = false;
+            Disciplina[] disciplinaGasita = {null};
+
             for (Disciplina d : Repository.getInstance().getDiscipline()) {
                 if (numeDisciplina.equals(d.getNumeDisciplina())) {
                     String linie = d.getNumeDisciplina() + " " + d.getCodDisciplina();
                     model.addElement(linie);
                     disciplinaGasit = true;
+                    disciplinaGasita[0] = d;
                 }
             }
+
             if (!disciplinaGasit) {
                 this.labelAfisareDiscipline.setText("Nu au fost găsite discipline cu acest nume.");
                 this.comboBox.setVisible(false);
@@ -40,20 +45,23 @@ public class ButonCautareDiscipline extends JButton implements Comand {
             this.comboBox.setModel(model);
             this.comboBox.setVisible(true);
 
+            // Stocăm disciplina selectată
+            this.disciplinaSelectata = disciplinaGasita[0];
+
             this.comboBox.addActionListener(e -> {
                 String selectedItem = (String) comboBox.getSelectedItem();
                 if (selectedItem != null) {
-                    this.labelAfisareDiscipline.setText("Disciplina selectata: " + selectedItem);
+                    this.labelAfisareDiscipline.setText("Disciplina selectată: " + selectedItem);
+                    this.disciplinaSelectata = disciplinaGasita[0];
                 }
             });
 
-            if (this.comboBox.getParent() == null) {
-                JPanel comboPanel = new JPanel();
-                comboPanel.add(this.comboBox);
-                this.labelAfisareDiscipline.getParent().add(comboPanel, BorderLayout.SOUTH);
-            }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public Disciplina getDisciplinaSelectata() {
+        return this.disciplinaSelectata;
     }
 }
